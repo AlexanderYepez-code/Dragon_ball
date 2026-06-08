99df9300df99c188c36a1a4065b23df762b861f2
AlexanderYepez-code <alexsaomn17@gmail.com>
2026-06-08 17:34:40 +0200

    pagina dei personaggi

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,18 @@ import { useState } from 'react';
 import './App.css';
 import { MantineProvider } from '@mantine/core';
 import '@mantine/core/styles.css';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter , Routes , Route} from 'react-router-dom';
 import { dragonBallTheme } from './theme';
 import { Navbar } from './components/Navbar';
+import Characterpage from './pages/Characterpage'
 function App() {
   return (
     <MantineProvider theme={dragonBallTheme} defaultColorScheme="dark">
       <BrowserRouter>
         <Navbar />
+        <Routes>
+          <Route path = "/personaggi" element={<Characterpage/>} />
+        </Routes>
       </BrowserRouter>
     </MantineProvider>
   );
