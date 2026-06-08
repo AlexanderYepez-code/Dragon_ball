d2ae67e05eb5f0c41b5ab67ad89ff282c63cf7e6
Kevin Napoli <kevinnapoli@outlook.it>
2026-06-08 16:32:37 +0200

    struttura

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import './App.css'
 
 function App() {
- 
+  return (
+    <MantineProvider theme={dragonBallTheme} defaultColorScheme="dark">
+
+
+      
+    </MantineProvider>
+  );
 }
 
 export default App
